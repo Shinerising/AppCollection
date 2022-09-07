49e23ab08cdbfa587019379b4ed48884ef59a856
Apollo Wayne <waynezhaoyang@gmail.com>
2022-09-07 19:46:28 +0800

    Merge Conflict

---
+++ src/app.ts
@@ -0,0 +1,129 @@
+import { Util, Data, DOM } from './common';
+
+HTMLElement.prototype.addClass = function (className: string) {
+  if (!this.classList.contains(className)) {
+    this.classList.add(className);
+  }
+};
+
+HTMLElement.prototype.removeClass = function (className: string) {
+  if (this.classList.contains(className)) {
+    this.classList.remove(className);
+  }
+};
+HTMLElement.prototype.toggleClass = function (className: string) {
+  if (!this.classList.contains(className)) {
+    this.classList.add(className);
+  } else {
+    this.classList.remove(className);
+  }
+};
+HTMLElement.prototype.val = function (value?: string) {
+  if (value === '') {
+    (<HTMLInputElement>this).value = '';
+  } else if (value) {
+    (<HTMLInputElement>this).value = value;
+  }
+  return (<HTMLInputElement>this).value;
+};
+
+/**
+ * @class App
+ * @description The main class of the application.
+ * @author Apollo Wayne
+ * @version 1.0.0
+ */
+export class App {
+
+  /**
+   * Start the app
+   */
+  public async start() {
+    await this.waitDocumentReady();
+
+    DOM.load();
+
+    this.startTypingTitle('Sic Parvis Magna.');
+
+    this.addScrollHandler();
+
+    Data.startFetching();
+
+    try {
+      await this.registerServiceWorker();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  /**
+   * Register Service Worker
+   */
+  private async registerServiceWorker() {
+    if ('serviceWorker' in navigator) {
+      //await navigator.serviceWorker.register('service-worker.js');
+    }
+  }
+
+  private async startTypingTitle(title: string) {
+    await Util.timeout(500);
+    DOM.query('#titleBox').style.opacity = '1';
+    DOM.query('#titleBack').textContent = title;
+    await Util.timeout(1000);
+    for (const char of title.split('')) {
+      await Util.timeout(50 + Math.round(Math.random() * 200));
+      DOM.titleBox.innerHTML += char === ' ' ? '&nbsp;' : char;
+    }
+  }
+
+
+  private addScrollHandler() {
+    let position = window.scrollY;
+    let ticking = false;
+
+    this.setScrollValue(position);
+
+    document.addEventListener('scroll', () => {
+      position = window.scrollY;
+
+      if (!ticking) {
+        window.requestAnimationFrame(() => {
+          this.setScrollValue(position);
+          ticking = false;
+        });
+
+        ticking = true;
+      }
+    });
+  }
+
+  private setScrollValue(offset: number) {
+    const blur = Math.min(3, Math.min(offset / 100, 20));
+    const blurImage = Math.min(5, Math.min(offset / 100, 20));
+    const blurText = Math.min(2, Math.min(offset / 100, 10) - 2);
+    const opacity = Math.max(0, 2 - Math.max(offset / 200, 1));
+    const filter = `saturate(180%) blur(${blurImage}rem)`;
+    DOM.coverGlass.setAttribute('style', `-webkit-backdrop-filter:${filter};backdrop-filter:${filter}`);
+    DOM.titleBox.setAttribute('style', `filter:blur(${blurText}rem);opacity:${opacity}`);
+    DOM.sitename.setAttribute('style', `filter:blur(${blur}rem);opacity:${opacity}`);
+  }
+
+  /**
+   * Wait for DOM ready
+   * @returns {Promise<void>}
+   */
+  private waitDocumentReady(): Promise<boolean> {
+    return new Promise<boolean>((resolve) => {
+      if (document.readyState === 'complete' || document.readyState === 'interactive') {
+        resolve(true);
+      } else {
+        const callback = () => {
+          document.removeEventListener('DOMContentLoaded', callback);
+          resolve(true);
+        };
+        document.addEventListener('DOMContentLoaded', callback);
+      }
+    });
+  }
+}
+
---
+++ src/common.ts
@@ -0,0 +1,132 @@
+import { RepoInfo, Repository, Release } from './app.interface';
+import { Config } from './config';
+
+type debounceFunction = (...args: string[]) => void;
+export class Util {
+  public static async timeout(delay: number): Promise<boolean> {
+    return new Promise<boolean>(resolve => {
+      setTimeout(() => {
+        resolve(true);
+      }, delay);
+    });
+  }
+  public static async nextFrame(): Promise<boolean> {
+    return new Promise<boolean>(resolve => {
+      requestAnimationFrame(() => {
+        resolve(true);
+      });
+    });
+  }
+  public static debounce(fn: debounceFunction, wait: number) {
+    let timeout: NodeJS.Timeout;
+
+    return (...args: string[]) => {
+      const later = () => {
+        clearTimeout;
+        fn(...args);
+      };
+
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  }
+  public static isURL(query: string) {
+    // eslint-disable-next-line
+    const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/ig;
+    return query.length < 2083 && urlRegex.test(query);
+  }
+}
+
+export class Data {
+
+  public static async startFetching() {
+    const list = await this.fetchData<RepoInfo[]>('list.json');
+
+    DOM.card.addClass('visible');
+
+    for (const item of list) {
+      const repo = await this.fetchData<Repository>(`${Config.API}\\${Config.Owner}\\${item.name}`);
+      const releases = await this.fetchData<Release[]>(`${Config.API}\\${Config.Owner}\\${item.name}\\releases`);
+      const node = this.generateNode(item, repo, releases);
+      this.applyNode(node);
+    }
+  }
+
+  private static async applyNode(node: string) {
+    DOM.collection.innerHTML += node;
+    await Util.nextFrame();
+    DOM.queryAll('section.repo').forEach(item => item.addClass('visible'));
+  }
+
+  private static generateNode(info:RepoInfo, repo: Repository, releases: Release[]): string {
+    const releaseNormal = releases.filter(item => !item.prerelease)[0];
+    const releasePreview = releases.filter(item => item.prerelease)[0];
+    const release = releaseNormal || releasePreview;
+    return `
+<section class="repo">
+<div class="repo-cover">
+<img src="https://raw.githubusercontent.com/${repo.full_name}/master/preview.jpg"/>
+</div>
+<div class="repo-info">
+<div class="repo-icon">
+<img src="https://raw.githubusercontent.com/${repo.full_name}/master/icon.png">
+</div>
+<h2 class="repo-title">${info.full_name || repo.name}</h2>
+<div class="repo-labelbox">
+<span>${repo.language}</span>${repo.topics.map(item => `<span>${item}</span>`).join('')}
+</div>
+<p class="repo-description">${repo.description}</p>
+</div>
+<div class="repo-link">
+<a class="link-github icon-github" href="${repo.html_url}">SOURCE CODE</a>
+<a class="link-github icon-tag" href="${release?.html_url || ''}">${release.prerelease ? 'PREVIEW' : release?.name.toUpperCase() || 'None'}</a>
+<a class="link-github icon-download" href="${release?.assets[0].browser_download_url || ''}">DOWNLOAD</a>
+</div>
+</section>
+    `;
+  }
+
+  private static async fetchData<T>(url: string): Promise<T> {
+    return await fetch(url).then((res) => res.json()).then(res => <T>res);
+  }
+}
+/**
+ * DOM API
+ */
+export class DOM {
+  public static sitename: HTMLElement;
+  public static titleBox: HTMLElement;
+  public static coverGlass: HTMLElement;
+  public static card: HTMLElement;
+  public static collection: HTMLElement;
+
+  /**
+   * Load common nodes
+   */
+  public static load() {
+    this.sitename = document.querySelector('#sitename') as HTMLElement;
+    this.titleBox = document.querySelector('#title') as HTMLElement;
+    this.coverGlass = document.querySelector('#cover>.cover-glass') as HTMLElement;
+    this.card = document.querySelector('.card') as HTMLElement;
+    this.collection = document.querySelector('.collection') as HTMLElement;
+  }
+
+  /**
+   * Select one node
+   * @param  {string} selector Selector
+   * @return {HTMLElement} DOM element
+   */
+  public static query(selector: string): HTMLElement {
+    return document.querySelector(selector) as HTMLElement;
+  }
+
+  /**
+   * Select multiple nodes
+   * @param  {string} selector Selector
+   * @return {NodeListOf<HTMLElement>} DOM Elements
+   */
+  public static queryAll(selector: string): NodeListOf<HTMLElement> {
+    return document.querySelectorAll(selector);
+  }
+}
+
