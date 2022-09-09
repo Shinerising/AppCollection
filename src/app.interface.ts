23d51f3b6d470bc7db9156fb8e9c1499a6e6d0e3
Apollo Wayne <waynezhaoyang@gmail.com>
2022-09-09 10:57:00 +0800

    Update Style

--- src/app.interface.ts
+++ src/app.interface.ts
@@ -4,6 +4,42 @@ export interface RepoInfo {
   document: string;
   platform: string;
 }
+export interface Asset {
+  url: string;
+  browser_download_url: string;
+  id: number;
+  node_id: string;
+  name: string;
+  label: string;
+  state: string;
+  content_type: string;
+  size: number;
+  download_count: number;
+  created_at: Date;
+  updated_at: Date;
+  uploader: Author;
+}
+
+export interface Author {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
 export interface Repository {
   id: number;
   node_id: string;
@@ -108,41 +144,4 @@ export interface Release {
   published_at: Date;
   author: Author;
   assets: Asset[];
-}
-
-export interface Asset {
-  url: string;
-  browser_download_url: string;
-  id: number;
-  node_id: string;
-  name: string;
-  label: string;
-  state: string;
-  content_type: string;
-  size: number;
-  download_count: number;
-  created_at: Date;
-  updated_at: Date;
-  uploader: Author;
-}
-
-export interface Author {
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
 }
--- src/common.ts
+++ src/common.ts
@@ -1,66 +1,47 @@
-import { RepoInfo, Repository, Release } from './app.interface';
-import { Config } from './config';
+import { RepoInfo, Repository, Release } from './app.interface'
+import { Config } from './config'
 
-type debounceFunction = (...args: string[]) => void;
 export class Util {
-  public static async timeout(delay: number): Promise<boolean> {
+  public static async timeout (delay: number): Promise<boolean> {
     return new Promise<boolean>(resolve => {
       setTimeout(() => {
-        resolve(true);
-      }, delay);
-    });
+        resolve(true)
+      }, delay)
+    })
   }
-  public static async nextFrame(): Promise<boolean> {
+
+  public static async nextFrame (): Promise<boolean> {
     return new Promise<boolean>(resolve => {
       requestAnimationFrame(() => {
-        resolve(true);
-      });
-    });
-  }
-  public static debounce(fn: debounceFunction, wait: number) {
-    let timeout: NodeJS.Timeout;
-
-    return (...args: string[]) => {
-      const later = () => {
-        clearTimeout;
-        fn(...args);
-      };
-
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-    };
-  }
-  public static isURL(query: string) {
-    // eslint-disable-next-line
-    const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/ig;
-    return query.length < 2083 && urlRegex.test(query);
+        resolve(true)
+      })
+    })
   }
 }
 
 export class Data {
+  public static async startFetching () {
+    const list = await this.fetchData<RepoInfo[]>('list.json')
 
-  public static async startFetching() {
-    const list = await this.fetchData<RepoInfo[]>('list.json');
-
-    DOM.card.addClass('visible');
+    DOM.card.addClass('visible')
 
     for (const item of list) {
-      const repo = await this.fetchData<Repository>(`${Config.API}/${Config.Owner}/${item.name}`);
-      const releases = await this.fetchData<Release[]>(`${Config.API}/${Config.Owner}/${item.name}/releases`);
-      const node = this.generateNode(item, repo, releases);
-      this.applyNode(node);
+      const repo = await this.fetchData<Repository>(`${Config.API}/${Config.Owner}/${item.name}`)
+      const releases = await this.fetchData<Release[]>(`${Config.API}/${Config.Owner}/${item.name}/releases`)
+      const node = this.generateNode(item, repo, releases)
+      this.applyNode(node)
     }
   }
 
-  private static async applyNode(node: string) {
-    DOM.collection.innerHTML += node;
-    await Util.nextFrame();
-    DOM.queryAll('section.repo').forEach(item => item.addClass('visible'));
+  private static async applyNode (node: string) {
+    DOM.collection.innerHTML += node
+    await Util.nextFrame()
+    DOM.queryAll('section.repo').forEach(item => item.addClass('visible'))
   }
 
-  private static generateNode(info:RepoInfo, repo: Repository, releases: Release[]): string {
-    const releaseNormal = releases.filter(item => !item.prerelease)[0];
-    const releasePreview = releases.filter(item => item.prerelease)[0];
+  private static generateNode (info:RepoInfo, repo: Repository, releases: Release[]): string {
+    const releaseNormal = releases.filter(item => !item.prerelease)[0]
+    const releasePreview = releases.filter(item => item.prerelease)[0]
     return `
 <section class="repo">
 <div class="repo-cover">
@@ -78,67 +59,57 @@ export class Data {
 </div>
 <p class="repo-description">${repo.description}</p>
 <ul class="repo-property">
-<li class="icon-monitor">${info.platform}</li>
-<li class="icon-clock">${this.timeDifference((new Date()).getTime(), new Date(repo.updated_at).getTime())}</li>
-<li class="icon-code">${this.getSize(repo.size)}</li>
-<li class="icon-tag">${releaseNormal?.tag_name.toUpperCase() || 'Release'}</li>
+<li class="icon-monitor" title="Platform">${info.platform}</li>
+<li class="icon-tag" title="Latest version">${releaseNormal?.tag_name.toUpperCase() || 'Preview'}</li>
+<li class="icon-code" title="Code size">${this.getSize(repo.size)}</li>
+<li class="icon-clock" title="Updated time">${this.timeDifference((new Date()).getTime(), new Date(repo.updated_at).getTime())}</li>
 </ul>
 </div>
 <div class="repo-link">
 <a class="link-github icon-book-open" href="${info.document}">Document</a>
 <a class="link-github icon-download" href="${releasePreview?.assets[0] ? `${releasePreview.assets[0].url.replace('https://api.github.com/repos', Config.Download)}?file=${releasePreview.assets[0].name}` : ''}">Preview</a>
-<a class="link-github icon-download" href="${releaseNormal?.assets[0]? `${releaseNormal.assets[0].url.replace('https://api.github.com/repos', Config.Download)}?file=${releaseNormal.assets[0].name}` : ''}">Package ${releaseNormal?.tag_name.toUpperCase() || 'Release'}</a>
+<a class="link-github icon-download" href="${releaseNormal?.assets[0] ? `${releaseNormal.assets[0].url.replace('https://api.github.com/repos', Config.Download)}?file=${releaseNormal.assets[0].name}` : ''}">Package ${releaseNormal?.tag_name.toUpperCase() || 'Release'}</a>
 </div>
 </section>
-    `;
+    `
   }
 
-  private static async fetchData<T>(url: string): Promise<T> {
-    return await fetch(url).then((res) => res.json()).then(res => <T>res);
+  private static async fetchData<T> (url: string): Promise<T> {
+    return await fetch(url).then((res) => res.json()).then(res => <T>res)
   }
 
-  private static getSize(size:number):string {
+  private static getSize (size:number):string {
     if (size < 1024) {
-      return size + 'KB'; 
+      return size + 'KB'
     }
-    return Math.round(size/1024) + 'MB';
+    return Math.round(size / 1024) + 'MB'
   }
 
-  private static timeDifference(current:number, previous:number):string {
-    const rtf = new Intl.RelativeTimeFormat(navigator.language || 'en');
+  private static timeDifference (current:number, previous:number):string {
+    const rtf = new Intl.RelativeTimeFormat(navigator.language || 'en')
 
-    const msPerMinute = 60 * 1000;
-    const msPerHour = msPerMinute * 60;
-    const msPerDay = msPerHour * 24;
-    const msPerMonth = msPerDay * 30;
-    const msPerYear = msPerDay * 365;
+    const msPerMinute = 60 * 1000
+    const msPerHour = msPerMinute * 60
+    const msPerDay = msPerHour * 24
+    const msPerMonth = msPerDay * 30
+    const msPerYear = msPerDay * 365
 
-    const elapsed = current - previous;
+    const elapsed = current - previous
 
     if (elapsed < msPerMinute) {
-         return rtf.format(Math.round(elapsed/1000)*-1,'second');   
+      return rtf.format(Math.round(elapsed / 1000) * -1, 'second')
+    } else if (elapsed < msPerHour) {
+      return rtf.format(Math.round(elapsed / msPerMinute) * -1, 'minute')
+    } else if (elapsed < msPerDay) {
+      return rtf.format(Math.round(elapsed / msPerHour) * -1, 'hour')
+    } else if (elapsed < msPerMonth) {
+      return rtf.format(Math.round(elapsed / msPerDay) * -1, 'day')
+    } else if (elapsed < msPerYear) {
+      return rtf.format(Math.round(elapsed / msPerMonth) * -1, 'month')
+    } else {
+      return rtf.format(Math.round(elapsed / msPerYear) * -1, 'year')
     }
-
-    else if (elapsed < msPerHour) {
-         return rtf.format(Math.round(elapsed/msPerMinute)*-1,'minute');     
-    }
-
-    else if (elapsed < msPerDay ) {
-         return rtf.format(Math.round(elapsed/msPerHour)*-1,'hour');   
-    }
-
-    else if (elapsed < msPerMonth) {
-        return rtf.format(Math.round(elapsed/msPerDay)*-1,'day');    
-    }
-
-    else if (elapsed < msPerYear) {
-        return rtf.format(Math.round(elapsed/msPerMonth)*-1,'month');    
-    }
-
-    else {
-        return rtf.format(Math.round(elapsed/msPerYear)*-1,'year');    
-    }
-}
+  }
 }
 /**
  * DOM API
@@ -153,12 +124,12 @@ export class DOM {
   /**
    * Load common nodes
    */
-  public static load() {
-    this.sitename = document.querySelector('#sitename') as HTMLElement;
-    this.titleBox = document.querySelector('#title') as HTMLElement;
-    this.coverGlass = document.querySelector('#cover>.cover-glass') as HTMLElement;
-    this.card = document.querySelector('.card') as HTMLElement;
-    this.collection = document.querySelector('.collection') as HTMLElement;
+  public static load () {
+    this.sitename = document.querySelector('#sitename') as HTMLElement
+    this.titleBox = document.querySelector('#title') as HTMLElement
+    this.coverGlass = document.querySelector('#cover>.cover-glass') as HTMLElement
+    this.card = document.querySelector('.card') as HTMLElement
+    this.collection = document.querySelector('.collection') as HTMLElement
   }
 
   /**
@@ -166,8 +137,8 @@ export class DOM {
    * @param  {string} selector Selector
    * @return {HTMLElement} DOM element
    */
-  public static query(selector: string): HTMLElement {
-    return document.querySelector(selector) as HTMLElement;
+  public static query (selector: string): HTMLElement {
+    return document.querySelector(selector) as HTMLElement
   }
 
   /**
@@ -175,8 +146,7 @@ export class DOM {
    * @param  {string} selector Selector
    * @return {NodeListOf<HTMLElement>} DOM Elements
    */
-  public static queryAll(selector: string): NodeListOf<HTMLElement> {
-    return document.querySelectorAll(selector);
+  public static queryAll (selector: string): NodeListOf<HTMLElement> {
+    return document.querySelectorAll(selector)
   }
 }
-
