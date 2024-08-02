7e8db098efaaaa4d64cec7547d8b5283301c0564
Apollo Wayne <waynezhaoyang@gmail.com>
2024-08-02 11:07:56 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export class Config {
   public static URL = "app.awayne.me"
   public static Owner = "Shinerising"
-  public static API = "https://github-api.crscd.net/api"
-  public static User = "https://github-api.crscd.net/users"
-  public static Content = "https://github-api.crscd.net/content"
-  public static Download = "https://github-api.crscd.net/download"
-  public static ImageProxy = "https://github-api.crscd.net/img"
+  public static API = "https://github-api.awayne.me/api"
+  public static User = "https://github-api.awayne.me/users"
+  public static Content = "https://github-api.awayne.me/content"
+  public static Download = "https://github-api.awayne.me/download"
+  public static ImageProxy = "https://github-api.awayne.me/img"
 }
